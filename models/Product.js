6df44c676bbdb6fa765a7b2e83849899dd69eda5
Emmanuel6093 <elorenzo6093@gmail.com>
2022-05-14 16:24:16 -0400

    added category id to product.js to be validated/categorised

--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,8 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+// import Category
+const Category = require('./Category'); 
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -21,8 +23,35 @@ Product.init(
     product_name: {
       type: DataTypes.STRING, 
       allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL(10,2), 
+      allowNull: false, 
+      validate: {
+        isDecimal: true 
+      }
+    }, 
+    // define stock column
+    stock: {
+      type: DataTypes.INTEGER, 
+      allowNull: false, 
+      defaultValue: 10, 
+      validate: {
+        isNumeric: true
+      }
+    }, 
+    // define category_id column
+    category_id: {
+      type: DataTypes.INTEGER, 
+      allowNull: false, 
+      references: {
+        model: 'category', 
+        key: 'id',
+      }
     }
   },
+  
+  
   {
     sequelize,
     timestamps: false,
