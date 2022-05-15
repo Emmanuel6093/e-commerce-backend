22469b2aa2929e76b4e06279a6a7ba6a5915ed1b
Emmanuel6093 <elorenzo6093@gmail.com>
2022-05-15 14:27:18 -0400

    category findOne route added

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -27,6 +27,28 @@ router.get('/:id', (req, res) => {
   // be sure to include its associated Products
 });
 
+Category.findOne({
+  where: {
+    id: req.params.id
+  },
+  attributes: ['id', 'category_name'], 
+  include: [{
+    model: 'product', 
+    attributes: ['id', 'product_name', 'price', 'stock', 'price', 'stock', 'category_name']
+  }]
+})
+.then(dbCategoryData => {
+  if (!dbCategoryData) {
+    res.status(404).json({ message: 'No category found with this id'}); 
+    return; 
+  }
+  res.json(dbCategoryData);
+})
+.catch(err => {
+  console.log(err);
+  res.status(500).json(err);
+});
+
 router.post('/', (req, res) => {
   // create a new category
 });
